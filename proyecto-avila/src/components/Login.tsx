86a0b8ab9cb1fe46fe5a10dd55312cbdf7ee5f2e
KikeBejarano <163807836+12010kike@users.noreply.github.com>
2025-03-10 12:07:54 -0400

    Interfaz Nueva

--- proyecto-avila/src/components/Login.tsx
+++ proyecto-avila/src/components/Login.tsx
@@ -41,7 +41,7 @@ const handleLogin = async (e: React.FormEvent) => {
 
   return (
     <div className="w-full bg-gray-100 flex-col items-center justify-center min-h-screen">
-      <div className="w-full bg-white rounded-lg shadow-lg p-8 border border-gray-400 text-black">
+      <div className="w-7/10 m-auto bg-white rounded-lg shadow-lg p-8 border border-gray-400 text-black">
       <button 
           onClick={() => navigate(-1)} 
           className="absolute left-4 top-4 text-gray-100 bg-gray-800 px-3 py-1 rounded-full hover:bg-gray-900"
--- proyecto-avila/src/components/VentanaActividades.tsx
+++ proyecto-avila/src/components/VentanaActividades.tsx
@@ -1,8 +1,27 @@
 import HeaderVentanas from './HeaderVentanas';
 import './VentanaActividades.css';
 
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../credentials";
+
+import avilaImage8 from '../images/el avila 21.png';
+
+type Actividad = {
+    id: string;
+    Guia: string;
+    horaInicio: string;
+    horaFinal: string;
+    Fecha: string;
+    personasInscritas: number;
+    limitesPersonas: number;
+    campamento: string;
+    Costo: string;
+    imagen: string[]; // Opcional, si quieres incluir imágenes
+  };
+
 const VentanaActividades: React.FC = () => {
-    const excursiones = [
+    /*const excursiones = [
         {
             guia: "Enrique León",
             horaInicio: "8:00 am (Sábado)",
@@ -25,7 +44,34 @@ const VentanaActividades: React.FC = () => {
             costo: "$20",
             imagenes: ["/img5.jpg", "/img6.jpg", "/img7.jpg", "/img8.jpg"],
         },
-    ];
+    ];*/
+
+
+    const [actividades, setActividades] = useState<Actividad[]>([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            // Referencia a la colección 'usuarios'
+            const actividadesRef = collection(db, "actividades");
+            const snapshot = await getDocs(actividadesRef);
+            
+            // Mapeo de los datos
+            const actividadesData = snapshot.docs.map(doc => ({
+            id: doc.id,
+
+            ...doc.data()
+            })) as Actividad[];
+    
+            setActividades(actividadesData);
+        } catch (error) {
+            console.error("Error al traer los datos:", error);
+        }
+        };
+    
+        fetchData();
+    }, []);
+
 
     return (
         <div className='page-container'>
@@ -43,23 +89,24 @@ const VentanaActividades: React.FC = () => {
                 />
             </div>
             
-            {excursiones.map((excursion, index) => (
+            {actividades.map((excursion, index) => (
                 <div key={index} className="excursion-card">
                     <div className="images-grid">
-                        {excursion.imagenes.slice(0, 3).map((img, i) => (
+                        {/*excursion.imagenes.slice(0, 3).map((img, i) => (
                             <img key={i} src={img} alt="Excursion" className="excursion-img" />
-                        ))}
+                        ))*/}
+                        <img src={avilaImage8} alt="" className="excursion-img"/>
                         <div className="ver-mas">Ver más...</div>
                     </div>
                     <div className="excursion-info">
-                        <p><strong>Guía:</strong> {excursion.guia}</p>
+                        <p><strong>Guía:</strong> {excursion.Guia}</p>
                         <p><strong>Hora Inicio:</strong> {excursion.horaInicio}</p>
                         <p><strong>Hora Final:</strong> {excursion.horaFinal}</p>
-                        <p><strong>Fecha:</strong> {excursion.fecha}</p>
-                        <p><strong>Personas inscritas:</strong> {excursion.inscritos}</p>
-                        <p><strong>Límite de personas:</strong> {excursion.limite}</p>
+                        <p><strong>Fecha:</strong> {excursion.Fecha}</p>
+                        <p><strong>Personas inscritas:</strong> {excursion.personasInscritas}</p>
+                        <p><strong>Límite de personas:</strong> {excursion.limitesPersonas}</p>
                         <p><strong>Campamento:</strong> {excursion.campamento}</p>
-                        <p><strong>Costo de la actividad:</strong> {excursion.costo}</p>
+                        <p><strong>Costo de la actividad:</strong> {excursion.Costo}</p>
                             <button className="anotarse">Anotarse</button>
                             <button className="detalles">Ver más detalles</button>
                         
