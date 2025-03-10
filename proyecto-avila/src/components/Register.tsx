8135cb1a8a93a1d938eb00174363b983ce29c28d
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2025-03-10 14:13:27 -0400

    Caracteres contraseña

--- proyecto-avila/src/components/Register.tsx
+++ proyecto-avila/src/components/Register.tsx
@@ -11,7 +11,6 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 const Register: React.FC = () => {
-
   const navigate = useNavigate();
 
   const goToAbout = () => {
@@ -26,47 +25,67 @@ const Register: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+  const [passwordError, setPasswordError] = useState<string | null>(null); // Estado para el mensaje de error
+
+  // Función para validar la contraseña
+  const validatePassword = (password: string): boolean => {
+    if (password.length < 6) {
+      setPasswordError("La contraseña debe tener al menos 6 caracteres.");
+      return false;
+    } else {
+      setPasswordError(null); // Limpiar el mensaje de error si la contraseña es válida
+      return true;
+    }
+  };
+
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    // Validar la contraseña antes de continuar
+    if (!validatePassword(password)) {
+      return; // Detener el proceso si la contraseña no es válida
+    }
+
     try {
+      const nombreRegistrado = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(nombreRegistrado.user.email);
 
-      const nombreRegistrado= await createUserWithEmailAndPassword(auth, email, password)
-
-      console.log(nombreRegistrado.user.email)
-
-      await setDoc( doc(db, 'users', nombreRegistrado.user.uid), {
+      await setDoc(doc(db, 'users', nombreRegistrado.user.uid), {
         nombre: name,
         email: email,
         uid: nombreRegistrado.user.uid,
-        fechaCreacion: new Date()
+        fechaCreacion: new Date(),
       });
-      setName("")
-      setEmail("")
-      setPassword("")
-      navigate("/")
 
+      setName("");
+      setEmail("");
+      setPassword("");
+      navigate("/");
     } catch (error) {
-
-      console.log(error)
-    
+      console.log(error);
     }
-
-  }
+  };
 
   return (
     <div className="w-full bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="w-7/10 m-auto bg-white rounded-lg shadow-lg p-8 border border-gray-400 text-black">
-        <button 
-          onClick={() => navigate(-1)} 
-          className="absolute left-4 top-4 text-gray-100 !bg-gray-800 px-3 py-1 rounded-full hover:!bg-gray-900">
-            ← Volver
+        <button
+          onClick={() => navigate(-1)}
+          className="absolute left-4 top-4 text-gray-100 !bg-gray-800 px-3 py-1 rounded-full hover:!bg-gray-900"
+        >
+          ← Volver
         </button>
         <h1 className="text-center text-2xl font-bold mb-2">AVILA EXS</h1>
         <p className="text-center text-lg mb-4">¡Únete a nuestra familia!</p>
         <div className="flex justify-center mb-4">
           <div className="relative">
-            <img alt="name for profile picture" className="w-24 h-24 rounded-full border-2 border-gray-300" height="100" src={avilaImage11} width="100"/>
+            <img
+              alt="name for profile picture"
+              className="w-24 h-24 rounded-full border-2 border-gray-300"
+              height="100"
+              src={avilaImage11}
+              width="100"
+            />
             <div className="absolute inset-0 flex items-center justify-center">
               <i className="fas fa-camera text-gray-700"></i>
             </div>
@@ -74,42 +93,99 @@ const Register: React.FC = () => {
         </div>
         <form onSubmit={handleRegister} className="space-y-4">
           <div className="mb-4">
-            <input value={name} onChange={(e)=> setName(e.target.value)} className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="name" type="text" placeholder="Nombre"/>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="name"
+              type="text"
+              placeholder="Nombre"
+            />
           </div>
           <div className="mb-4">
-            <input className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="Apellido" type="text" placeholder="Apellido"/>
+            <input
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="Apellido"
+              type="text"
+              placeholder="Apellido"
+            />
           </div>
           <div className="mb-4">
-            <input className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="Edad" type="text" placeholder="Edad"/>
+            <input
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="Edad"
+              type="text"
+              placeholder="Edad"
+            />
           </div>
           <div className="mb-4">
-            <input value={email} onChange={(e)=> setEmail(e.target.value)} className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="email" type="email" placeholder="Correo electrónico"/>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="email"
+              type="email"
+              placeholder="Correo electrónico"
+            />
           </div>
           <div className="mb-4">
-            <input className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="Número telefónico" type="tel" placeholder="Número telefónico"/>
+            <input
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="Número telefónico"
+              type="tel"
+              placeholder="Número telefónico"
+            />
           </div>
           <div className="mb-4">
-            <input value={password} onChange={(e)=> setPassword(e.target.value)} className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="password" type="password" placeholder="Nueva contraseña"/>
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                validatePassword(e.target.value); // Validar la contraseña en tiempo real
+              }}
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="password"
+              type="password"
+              placeholder="Nueva contraseña, mínimo 6 caracteres"
+            />
+            {passwordError && (
+              <p className="text-red-500 text-sm mt-1">{passwordError}</p>
+            )}
           </div>
           <div className="mb-4">
-            <input className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700" name="Confirmar contraseña" type="password" placeholder="Confirmar contraseña"/>
+            <input
+              className="w-full px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="Confirmar contraseña"
+              type="password"
+              placeholder="Confirmar contraseña"
+            />
           </div>
           <div className="mb-4">
-            <textarea className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-700" name="Habla sobre tus habilidades, experiencias y el por qué deberías ser un guía aquí..." placeholder="Habla sobre tus habilidades, experiencias y el por qué deberías ser un guía aquí..."></textarea>
+            <textarea
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-700"
+              name="Habla sobre tus habilidades, experiencias y el por qué deberías ser un guía aquí..."
+              placeholder="Habla sobre tus habilidades, experiencias y el por qué deberías ser un guía aquí..."
+            ></textarea>
           </div>
           <div className="flex justify-center">
-            <button className="w-full !bg-gray-800 text-gray-100 py-2 rounded-full hover:!bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-700" type="submit">
+            <button
+              className="w-full !bg-gray-800 text-gray-100 py-2 rounded-full hover:!bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-700"
+              type="submit"
+            >
               Registrarse
             </button>
           </div>
         </form>
         <div className="flex justify-center mt-4">
-            <div className="w-full">
-              <GoogleAuth onLoginSuccess={handleGoogleLoginSuccess} />
-            </div>
+          <div className="w-full">
+            <GoogleAuth onLoginSuccess={handleGoogleLoginSuccess} />
+          </div>
         </div>
         <p className="text-center mt-4">
-          ¿Ya tienes cuenta? <span className="text-green-600 hover:underline" onClick={goToAbout}>Log In</span>
+          ¿Ya tienes cuenta?{' '}
+          <span className="text-green-600 hover:underline" onClick={goToAbout}>
+            Log In
+          </span>
         </p>
       </div>
     </div>
