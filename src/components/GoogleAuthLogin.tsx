7da293d84c24b076e9b93b8a4392773fa20c94fe
valenvvegasv <vegas.valentina@correo.unimet.edu.ve>
2025-03-11 21:54:31 -0400

    login y register con google

--- src/components/GoogleAuthLogin.tsx
+++ src/components/GoogleAuthLogin.tsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
-import { app } from '../credentials'; 
+import React from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { app } from "../credentials";
+
 
 const db = getFirestore(app);
+
 
 interface GoogleAuthLoginProps {
   onSuccess: () => void;
 }
 
+
 const GoogleAuthLogin: React.FC<GoogleAuthLoginProps> = ({ onSuccess }) => {
   const handleGoogleLogin = async () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    
+
+
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
 
-      // Guarda los datos del usuario en Firestore
-      await setDoc(doc(db, 'users', user.uid), {
-        uid: user.uid,
-        email: user.email,
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-      });
 
-      console.log('Usuario autenticado y datos guardados en Firestore:', user.uid);
-      onSuccess(); // Llamar a la función de éxito
+      // Verifica si el usuario existe en Firestore
+      const userRef = doc(db, "users", user.uid);
+      const userSnap = await getDoc(userRef);
 
+
+      if (userSnap.exists()) {
+        console.log("Inicio de sesión exitoso:", user.uid);
+        onSuccess(); // Llamar a la función de éxito (redirigir, cambiar estado, etc.)
+      } else {
+        alert("Este correo no está registrado. Por favor, regístrate primero.");
+      }
     } catch (error) {
-      console.error('Error durante la autenticación:', error);
+      console.error("Error durante la autenticación:", error);
     }
   };
 
+
   return (
-    <button 
-      onClick={handleGoogleLogin} 
+    <button
+      onClick={handleGoogleLogin}
       className="w-full text-gray-100 !bg-red-500 px-3 py-1 rounded-full hover:!bg-red-600"
     >
       Iniciar sesión con Google
@@ -44,7 +50,6 @@ const GoogleAuthLogin: React.FC<GoogleAuthLoginProps> = ({ onSuccess }) => {
   );
 };
 
+
 export default GoogleAuthLogin;
-
-
 
