81552af033248a838abaf0227caffc6f22d57fe5
12010kike <enrique.bejarano@correo.unimet.edu.ve>
2025-03-19 11:46:59 -0400

    Responsive en el headerAdmin

--- src/components/HeaderAdmin.tsx
+++ src/components/HeaderAdmin.tsx
@@ -33,7 +33,7 @@ const Header: React.FC = () => {
 
   return (
     <header className="w-full bg-white shadow-md">
-      <div className="hidden md:flex justify-around pt-2.5 pb-2.5">
+      <div className="hidden lg:flex justify-around pt-2.5 pb-2.5">
         <motion.button 
           className="login-button flex items-center gap-2 p-2 bg-green-600 text-white rounded-full hover:bg-green-700"
           whileHover={{ scale: 1.1 }}
@@ -84,6 +84,61 @@ const Header: React.FC = () => {
             </motion.button>
           </>
         )}
+      </div>
+      <div className="flex lg:hidden flex-col justify-around pt-2.5 pb-2.5">
+        <div className='flex mb-5 justify-center '>
+          <motion.button 
+            className="login-button flex items-center gap-2 p-2 bg-green-600 text-white rounded-full hover:bg-green-700"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src={avilaImage12} alt="Perfil" className="w-6 h-8" />
+            <strong className="login-button flex text-lg items-center font-bold gap-2 px-4 py-2 !bg-teal-700 !text-white rounded-full transition-all duration-200 transform hover:scale-105 hover:!bg-[#174f4f]"> AVILA EXS </strong>
+          </motion.button>
+
+          {logged ? (
+            <>
+              <div className="flex gap-4">
+                <motion.button 
+                  className="login-button flex items-center gap-2 p-2 !bg-teal-700 !text-white rounded-full hover:!bg-[#174f4f]"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  <img src={avilaImage11} alt="Perfil" className="w-6 h-6 rounded-full" />
+                  Admin
+                </motion.button>
+
+                <motion.button 
+                  className="login-button flex items-center gap-2 p-2 !bg-teal-700 !text-white rounded-full hover:!bg-[#174f4f]" 
+                  onClick={handleLogout}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  Log Out
+                </motion.button>
+              </div>
+            </>
+          ) : (
+            <>
+              <motion.button 
+                className="login-button" 
+                onClick={goToAbout}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Log In
+              </motion.button>
+            </>
+          )}
+        </div>
+        <nav className="flex justify-around space-x-4 bg-gray-300 bg-opacity-50 rounded-full px-4 py-2 font-semibold mx-2 mb-2">
+            <button className="!bg-transparent px-2 py-2 !rounded-full hover:!bg-[#1d6363] hover:!text-white" onClick={() => navigate('/')}>Principal</button> 
+            <button className="!bg-transparent px-2 py-2 !rounded-full hover:!bg-[#1d6363] hover:!text-white" onClick={() => navigate('/ventana-actividades')}>Actividades</button>
+            <button className="!bg-transparent px-2 py-2 !rounded-full hover:!bg-[#1d6363] hover:!text-white" onClick={goToAbout}>Reservas</button>
+            <button className="!bg-transparent px-2 py-2 !rounded-full hover:!bg-[#1d6363] hover:!text-white" onClick={() => navigate('/foro')}>Foro</button>
+        </nav>
+
+        
       </div>
     </header>
   );
